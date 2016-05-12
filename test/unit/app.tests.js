1e9e398e8d0a9e48313e0ea7326f1e8177566729
ezequiel <ezequielc@gmail.com>
2016-05-12 16:24:23 -0300

    External link to access Priority Support

---
+++ test/unit/app.tests.js
@@ -0,0 +1,49 @@
+'use strict';
+  
+describe('app:', function() {
+  beforeEach(function () {    
+      angular.module('risevision.apps.partials',[]);
+
+      module('risevision.apps');
+
+      module(function ($provide) {
+        $provide.service('canAccessApps',function(){
+          return function() {
+            var deferred = Q.defer();
+            deferred.resolve("auth");
+            return deferred.promise;
+          }
+        });
+      });
+
+      inject(function ($injector) {
+        $state = $injector.get('$state');
+        supportFactory = $injector.get('supportFactory');
+        canAccessApps = $injector.get('canAccessApps');
+      });
+  });
+
+  
+  var $state, supportFactory, canAccessApps;
+
+  describe('state apps.launcher.support:',function(){
+    
+    it('should register state',function(){
+      var supportState = $state.get('apps.launcher.support')
+      expect(supportState).to.be.ok;
+      expect(supportState.url).to.equal('/support');
+      expect(supportState.controller).to.be.ok;
+    });
+
+    it('should init support modal',function(done){
+      var spy = sinon.spy(supportFactory,'handlePrioritySupportAction') 
+      $state.get('apps.launcher.support').controller[3]($state, canAccessApps, supportFactory);
+      setTimeout(function() {
+        spy.should.have.been.called;
+        done();
+      }, 10);
+    });
+
+  });  
+
+});
