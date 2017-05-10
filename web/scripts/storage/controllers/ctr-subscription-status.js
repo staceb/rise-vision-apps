fe0fff971cbf51e4e1c7a30786e46a0b44f05866
Francisco Vallarino <fjvallarino@users.noreply.github.com>
2017-05-10 21:40:29 +0200

    Use new pricing page for Storage subscription link (#421)
    * Update common-header to get latest component-subscription-status
    
    * Use custom link for Storage Subscription status
    
    * Use latest common-header

--- web/scripts/storage/controllers/ctr-subscription-status.js
+++ web/scripts/storage/controllers/ctr-subscription-status.js
@@ -3,13 +3,15 @@
 angular.module('risevision.storage.controllers')
   .value('STORAGE_PRODUCT_CODE', 'b0cba08a4baa0c62b8cdc621b6f6a124f89a03db')
   .value('STORAGE_PRODUCT_ID', '24')
+  .value('STORAGE_PRODUCT_LINK', 'https://www.risevision.com/pricing?utm_campaign=apps')
   .controller('SubscriptionStatusController', ['$scope', '$rootScope',
-    'userState', 'STORAGE_PRODUCT_CODE', 'STORAGE_PRODUCT_ID',
+    'userState', 'STORAGE_PRODUCT_CODE', 'STORAGE_PRODUCT_ID', 'STORAGE_PRODUCT_LINK',
     function ($scope, $rootScope, userState,
-      STORE_PRODUCT_CODE, STORE_PRODUCT_ID) {
+      STORAGE_PRODUCT_CODE, STORAGE_PRODUCT_ID, STORAGE_PRODUCT_LINK) {
       $scope.companyId = userState.getSelectedCompanyId();
-      $scope.productCode = STORE_PRODUCT_CODE;
-      $scope.productId = STORE_PRODUCT_ID;
+      $scope.productCode = STORAGE_PRODUCT_CODE;
+      $scope.productId = STORAGE_PRODUCT_ID;
+      $scope.productLink = STORAGE_PRODUCT_LINK;
       $scope.subscriptionStatus = {};
 
       $rootScope.$on('selectedCompanyChanged', function (event, companyId) {
