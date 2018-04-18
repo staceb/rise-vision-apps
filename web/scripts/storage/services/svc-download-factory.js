010f81f76a1fb36f18b6bbb593e7100113f93255
Alex Deaconu <alex.deaconu@risevision.com>
2018-04-18 11:34:27 -0400

    Correcting header according to google documentation (#609)
    response-content-disposition header has its own syntax
    https://cloud.google.com/storage/docs/xml-api/reference-headers#responsecontentdisposition
    
    [stage-3]

--- web/scripts/storage/services/svc-download-factory.js
+++ web/scripts/storage/services/svc-download-factory.js
@@ -21,9 +21,8 @@ angular.module('risevision.storage.services')
         iframe.id = hiddenIFrameID;
         iframe.style.display = 'none';
         iframeContainer.appendChild(iframe);
-        // Using character encoding for the filename
-        // (https://tools.ietf.org/html/rfc8187#section-3.2.3)
-        iframe.src = url + '&response-content-disposition=attachment;filename*=UTF-8' + fileName;
+        // https://cloud.google.com/storage/docs/xml-api/reference-headers#responsecontentdisposition
+        iframe.src = url + '&response-content-disposition=attachment%3B%20filename%3D%22' + fileName + '%22';
       };
 
       var downloadFile = function (file) {
@@ -36,7 +35,7 @@ angular.module('risevision.storage.services')
                 '/') + 1);
             }
 
-            downloadURL(resp.message, encodeURIComponent(downloadName));
+            downloadURL(resp.message, encodeURIComponent(downloadName.replace(/\"/g, '\\"')));
           } else {
             file.rejectedUploadMessage = resp.message;
             svc.rejectedUploads.push(file);
