fb596b894892d47960d53d03b2b56eff1864da49
Rudi Bravo <rudibravo@gmail.com>
2018-08-10 10:11:23 -0300

    Electron player is returning true on isChromeOSPlayer check [stage-6]

--- test/unit/displays/services/svc-player-pro-factory.tests.js
+++ test/unit/displays/services/svc-player-pro-factory.tests.js
@@ -93,6 +93,8 @@ describe('service: playerProFactory:', function() {
     expect(playerProFactory.isChromeOSPlayer({playerName: null})).to.be.false;
     expect(playerProFactory.isChromeOSPlayer({playerName:'RisePlayer'})).to.be.false;
     expect(playerProFactory.isChromeOSPlayer({playerName:'RisePlayer', playerVersion: '2017.07.17.20.21'})).to.be.false;
+    expect(playerProFactory.isChromeOSPlayer({playerName:'RisePlayerElectron'})).to.be.false;
+    expect(playerProFactory.isChromeOSPlayer({playerName:'RisePlayerElectron', playerVersion: '2018.08.17.20.21'})).to.be.false;
     expect(playerProFactory.isChromeOSPlayer({playerName:'RisePlayer', playerVersion: '2018.08.17.20.21'})).to.be.true;
     expect(playerProFactory.isChromeOSPlayer({playerName:'(Beta) RisePlayer', playerVersion: '2018.08.17.20.21'})).to.be.true;
   });
