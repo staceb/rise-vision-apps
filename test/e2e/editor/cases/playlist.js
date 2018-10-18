cb3a3de8da64ecfea1035faf35276d3c2db7f30a
Francisco Vallarino <fjvallarino@users.noreply.github.com>
2018-10-18 14:25:35 -0300

    Temporarily disable Editor Playlist e2e tests (#765)
    * Disable Editor Playlist Items e2e tests [stage-4]
    
    * Temporarily disable Editor Playlist e2e tests [stage-4]

--- test/e2e/editor/cases/playlist.js
+++ test/e2e/editor/cases/playlist.js
@@ -17,7 +17,7 @@ var helper = require('rv-common-e2e').helper;
 var PlaylistScenarios = function() {
 
   browser.driver.manage().window().setSize(1920, 1080);
-  describe('Playlist', function () {
+  xdescribe('Playlist', function () {
     var homepage;
     var signInPage;
     var commonHeaderPage;
