b35d920b55f47e8f1991d9486ee45f65e0520ec9
Stuart Lees <stuartlees@gmail.com>
2019-02-23 00:27:29 -0500

    Implement presentation name editing UI and logic [stage-8]

--- web/scripts/template-editor/directives/dtv-toolbar.js
+++ web/scripts/template-editor/directives/dtv-toolbar.js
@@ -6,8 +6,37 @@ angular.module('risevision.template-editor.directives')
       return {
         restrict: 'E',
         templateUrl: 'partials/template-editor/toolbar.html',
-        link: function ($scope) {
+        link: function ($scope, element) {
           $scope.factory = templateEditorFactory;
+          $scope.isEditingName = false;
+
+          $scope.onPresentationNameBlur = function() {
+            $scope.isEditingName = false;
+          };
+
+          $scope.$watch("isEditingName", function(editing) {
+            var templateNameInput = element.find('input.presentation-name');
+
+            if (editing) {
+              setFocusPosition(templateNameInput[0], templateNameInput[0].value.length);
+            }
+          });
+
+          function setFocusPosition(elem, caretPos) {
+            if (elem !== null) {
+              if (elem.createTextRange) {
+                var range = elem.createTextRange();
+                range.move('character', caretPos);
+                range.select();
+              } else {
+                if (elem.setSelectionRange) {
+                  elem.focus();
+                  elem.setSelectionRange(caretPos, caretPos);
+                } else
+                  elem.focus();
+              }
+            }
+          }
         }
       };
     }
