43d7ed34bc7a456135010f181c1db2705899428d
Alex Deaconu <alex.deaconu@gmail.com>
2019-06-05 09:41:59 -0400

    Temporarily disable storage download e2e test (#1013)

--- test/e2e/storage/storage.cases.js
+++ test/e2e/storage/storage.cases.js
@@ -6,7 +6,7 @@
   var UploadScenarios = require('./cases/upload.js');
   var HomeScenarios = require('./cases/home.js');
   var CopyUrlScenarios = require('./cases/copy-url.js');
-  var DownloadScenarios = require('./cases/download.js');
+  // var DownloadScenarios = require('./cases/download.js');
   var TrashScenarios = require('./cases/trash.js');
   var IframeScenarios = require('./cases/iframe.js');
   var DismissModalScenarios = require('./cases/dismiss-modal.js');
@@ -18,7 +18,7 @@
     var uploadScenarios = new UploadScenarios();
     var homeScenarios = new HomeScenarios();
     var copyUrlScenarios = new CopyUrlScenarios();
-    var downloadScenarios = new DownloadScenarios();
+    // var downloadScenarios = new DownloadScenarios();
     var trashScenarios = new TrashScenarios();
     var iframeScenarios = new IframeScenarios();
     var dismissModalScenarios = new DismissModalScenarios();
