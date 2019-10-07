e4f38838a28143b196605a1787dae984d5ab46e5
Stuart Lees <stulees@users.noreply.github.com>
2019-10-07 15:49:21 -0400

    Open counter settings [stage-8] (#1293)

---
+++ web/scripts/template-editor/components/directives/dtv-component-counter.js
@@ -0,0 +1,27 @@
+'use strict';
+
+angular.module('risevision.template-editor.directives')
+  .directive('templateComponentCounter', ['templateEditorFactory',
+    function (templateEditorFactory) {
+      return {
+        restrict: 'E',
+        scope: true,
+        templateUrl: 'partials/template-editor/components/component-counter.html',
+        link: function ($scope, element) {
+          $scope.factory = templateEditorFactory;
+
+          $scope.registerDirective({
+            type: 'rise-data-counter',
+            iconType: 'streamline',
+            icon: 'hourglass',
+            element: element,
+            show: function () {
+              element.show();
+              $scope.componentId = $scope.factory.selected.id;
+            }
+          });
+
+        }
+      };
+    }
+  ]);
