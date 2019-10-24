7a297d6732b7db466e39f85bb755cf352c699e28
Francisco Vallarino <fjvallarino@users.noreply.github.com>
2019-10-24 17:05:02 -0300

    Show date picker in counter component (#1321)
    * First attempts at getting date component styled correctly. Also some temporary ugly hacks [stage-7]
    
    * Improve styling, save changes [stage-7]
    
    * Do not show grayed out input field for date picker
    
    * Handle counter type (down/up). Improve metadata handling [stage-7]

--- web/scripts/template-editor/components/directives/dtv-component-counter.js
+++ web/scripts/template-editor/components/directives/dtv-component-counter.js
@@ -7,23 +7,64 @@ angular.module('risevision.template-editor.directives')
         restrict: 'E',
         scope: true,
         templateUrl: 'partials/template-editor/components/component-counter.html',
-        link: function ($scope, element) {
-          $scope.factory = templateEditorFactory;
+        compile: function () {
+          return {
+            pre: function($scope) {
+              $scope.dateOptions = {
+                formatYear: 'yy',
+                startingDay: 1,
+                showWeeks: false,
+                showButtonBar: false
+              };
 
-          $scope.registerDirective({
-            type: 'rise-data-counter',
-            iconType: 'streamline',
-            icon: 'hourglass',
-            element: element,
-            show: function () {
-              element.show();
-              $scope.componentId = $scope.factory.selected.id;
+              $scope.targetDatePicker = {
+                isOpen: false,
+                initDate: null
+              };
             },
-            getTitle: function (component) {
-              return 'template.rise-data-counter' + '-' + component.attributes.type.value;
-            }
-          });
+            post: function ($scope, element) {
+              $scope.factory = templateEditorFactory;
 
+              $scope.registerDirective({
+                type: 'rise-data-counter',
+                iconType: 'streamline',
+                icon: 'hourglass',
+                element: element,
+                show: function () {
+                  $scope.componentId = $scope.factory.selected.id;
+                  _load();
+                },
+                getTitle: function (component) {
+                  return 'template.rise-data-counter' + '-' + component.attributes.type.value;
+                }
+              });
+
+              function _load () {
+                $scope.counterType = $scope.getAvailableAttributeData($scope.componentId, 'type');
+                $scope.targetDate = $scope.getAvailableAttributeData($scope.componentId, 'date');
+                $scope.targetTime = $scope.getAvailableAttributeData($scope.componentId, 'time');
+
+                $scope.targetUnit = $scope.targetTime ? 'targetTime' : 'targetDate';
+              }
+
+              $scope.save = function () {
+                if ($scope.targetUnit === 'targetDate') {
+                  $scope.setAttributeData($scope.componentId, 'date', $scope.targetDate);
+                  $scope.setAttributeData($scope.componentId, 'time', null);
+                } else {
+                  $scope.setAttributeData($scope.componentId, 'date', null);
+                  $scope.setAttributeData($scope.componentId, 'time', $scope.targetTime);
+                }
+              };
+
+              $scope.openDatePicker = function ($event) {
+                $event.preventDefault();
+                $event.stopPropagation();
+
+                $scope.targetDatePicker.isOpen = !$scope.targetDatePicker.isOpen;
+              };
+            }
+          };
         }
       };
     }
