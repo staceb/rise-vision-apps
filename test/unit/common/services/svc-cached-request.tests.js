703944adcc8535b32b730600d73cabd849e4eb9e
Alex Deaconu <alex.deaconu@gmail.com>
2019-12-12 11:39:12 -0500

    Update cached-request to handle concurrent requests
    Use a cached promise for the response
    
    Add reset functionality so API is not called again
    
    [stage-2]

--- test/unit/common/services/svc-cached-request.tests.js
+++ test/unit/common/services/svc-cached-request.tests.js
@@ -70,6 +70,76 @@ describe('service: CachedRequest', function() {
         done();
       });
     });
+
+  });
+
+  describe('reset:', function() {
+    it('should not request again on reset', function(done) {
+      cachedRequest.execute();
+      cachedRequest.reset();
+
+      setTimeout(function() {
+        expect(request).to.have.been.calledOnce;
+
+        done();
+      }, 10);
+    });
+
+    it('should make the request again if api is called after reset', function(done) {
+      cachedRequest.execute().then(function(resp){
+        expect(resp).to.equal('OK');
+        expect(request).to.have.been.calledOnce;
+
+        cachedRequest.reset();
+
+        cachedRequest.execute().then(function(cachedResp){
+          expect(request).to.have.been.calledTwice;
+          done();
+        });
+      });
+    });
+  });
+
+  describe('promise caching:', function() {
+    it('should only call API once on concurrent requests',function(done){
+      var response = Q.defer();
+      request.returns(response.promise);
+      var promiseResolve = sandbox.stub();
+
+      cachedRequest.execute().then(promiseResolve);
+      cachedRequest.execute().then(promiseResolve);
+
+      response.resolve('OK');
+
+      setTimeout(function() {
+        expect(request).to.have.been.calledOnce;
+        expect(promiseResolve).to.have.been.calledTwice;
+
+        done();
+      }, 10);
+    });
+
+    it('should resolve correct promise on reset',function(done){
+      var response = Q.defer();
+      request.returns(response.promise);
+      var promiseResolve1 = sandbox.stub();
+      var promiseResolve2 = sandbox.stub();
+
+      cachedRequest.execute().then(promiseResolve1);
+      cachedRequest.reset();
+      cachedRequest.execute().then(promiseResolve2);
+
+      response.resolve('OK');
+
+      setTimeout(function() {
+        expect(request).to.have.been.calledTwice;
+        expect(promiseResolve1).to.have.been.calledOnce;
+        expect(promiseResolve2).to.have.been.calledOnce;
+
+        done();
+      }, 10);
+    });
+
   });
 
 });
