4bacd487da6b3cffa1119e952bec997c377ba534
Francisco Vallarino <fjvallarino@users.noreply.github.com>
2020-02-13 15:43:00 +0100

    Add fontsize slider on rise-text UI (#1505)
    * Add fontsize slider [stage-7]
    
    * Fix e2e issue [stage-7]
    
    * Use Number to set slider's value [stage-7]
    
    * Only show fontsize slider if template indicated a default value for fontsize attribute [stage-7]

--- web/scripts/template-editor/components/directives/dtv-component-text.js
+++ web/scripts/template-editor/components/directives/dtv-component-text.js
@@ -1,8 +1,8 @@
 'use strict';
 
 angular.module('risevision.template-editor.directives')
-  .directive('templateComponentText', ['templateEditorFactory',
-    function (templateEditorFactory) {
+  .directive('templateComponentText', ['templateEditorFactory', 'templateEditorUtils',
+    function (templateEditorFactory, templateEditorUtils) {
       return {
         restrict: 'E',
         scope: true,
@@ -11,11 +11,20 @@ angular.module('risevision.template-editor.directives')
           $scope.factory = templateEditorFactory;
 
           function _load() {
+            var fontsize = $scope.getAvailableAttributeData($scope.componentId, 'fontsize');
+            var fontsizeInt = templateEditorUtils.intValueFor(fontsize, null);
+
             $scope.value = $scope.getAvailableAttributeData($scope.componentId, 'value');
+            $scope.fontsize = fontsizeInt;
+            $scope.showFontSize = !!fontsizeInt;
           }
 
           $scope.save = function () {
             $scope.setAttributeData($scope.componentId, 'value', $scope.value);
+
+            if ($scope.showFontSize) {
+              $scope.setAttributeData($scope.componentId, 'fontsize', $scope.fontsize);
+            }
           };
 
           $scope.registerDirective({
