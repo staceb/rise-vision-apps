15ed88509786d7fce11a0783e9867ecadce9c1c2
Ezequiel Conte <ezequielc@gmail.com>
2020-04-21 16:29:54 -0300

    Revert "Always show Schedule Preview button - No longer hides for HTML Templates (#1644)" (#1646)
    This reverts commit 95b12d54ec15ab78837feb805960d67d0a86df28.
    
    [stage-1]

--- web/scripts/schedules/directives/dtv-schedule-fields.js
+++ web/scripts/schedules/directives/dtv-schedule-fields.js
@@ -40,6 +40,14 @@ angular.module('risevision.schedules.directives')
               }
             });
           };
+
+          $scope.isPreviewAvailable = function () {
+            var htmlPresentations = _.filter($scope.schedule.content, function (presentation) {
+              return presentationUtils.isHtmlPresentation(presentation);
+            });
+
+            return htmlPresentations.length === 0;
+          };
         } //link()
       };
     }
